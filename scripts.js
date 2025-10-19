cf6934c56b735fc96b697fae281a61cd4753799f
Di-Anne Wyngaard <dianneleighwyngaard@gmail.com>
2025-10-19 21:54:13 +0200

    Add comments for sections

--- scripts.js
+++ scripts.js
@@ -25,19 +25,18 @@ while (initialTasks.length < 6) {
   addTask();
 }
 
+// Alert user when inital tasks' array reached its limit
 if (initialTasks.length === 6) {
   alert(
     "There are enough tasks on your board. Please check them in the console.",
   );
 }
 
-// User may add new task vua prompt and only 'todo', 'doing' or 'done' may be used as status inputs
+// Prompt user to input task and only 'todo', 'doing' or 'done' may be used as status inputs
 function addTask() {
   const taskTitle = prompt("Enter task title:");
   const taskDescription = prompt("Enter task description:");
-  let taskStatus = prompt(
-    "Enter task status (todo, doing, done):",
-  ).toLowerCase();
+  let taskStatus = prompt("Enter task status (todo, doing, done):").toLowerCase();
 
   // Loop until user enters valid status input: 'todo', 'doing', or 'done'
   while (
@@ -51,23 +50,23 @@ function addTask() {
 
   // Store user input values in an object
   const newTask = {
-    id: initialTasks.length + 1, //Add 1 from previous total length because ID will increase by 1 everytime a new task is added
+    id: initialTasks.length + 1, // Increase ID by 1 everytime a new task is added to indicate 6 ID's
     title: taskTitle,
     description: taskDescription,
     status: taskStatus,
   };
 
-  // Add new tasks to initial tasks array
+  // Push every new task to initial tasks array
   initialTasks.push(newTask);
 }
 
-//Filter all completed tasks in an array
+// Filter all completed tasks in an array
 function findcompletedTasks() {
   return initialTasks.filter((task) => task.status === "done");
 }
 
-//Display ALL tasks in console
+//Display all tasks in the console
 console.log("All tasks: ", initialTasks);
 
-//Display completed tasks in console
+//Display completed tasks in the console
 console.log("Completed tasks: ", findcompletedTasks());
